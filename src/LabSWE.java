d4ce922fc44e72967c3179f0fb7628e74e76b782
FilippoSciammacca <filipposciammacca@libero.it>
2022-10-12 18:20:50 +0200

    LabSWE: adesso cerca un Customer per ID o info complete e poi ci esegue azioni sopra, mentre prima decideva che voleva modificare l'oggetto e poi lo cercava. Aggiunto clientOptions(), che permette di svolgere delle operazioni su un oggetto Customer (attualmente solo la modifica)

--- src/LabSWE.java
+++ src/LabSWE.java
@@ -1,4 +1,5 @@
 import DAO.CustomerDAO;
+import model.Customer;
 
 import java.sql.SQLException;
 import java.util.Scanner;
@@ -18,7 +19,7 @@ public class LabSWE {
                 case 1 -> {
                     System.out.println("\t-- CLIENTE --");
                     System.out.println("\t 1 - Aggiungi nuovo cliente");
-                    System.out.println("\t 2 - Modifica dati cliente");
+                    System.out.println("\t 2 - Trova e modifica cliente");
                     System.out.println("\t 3 - Torna indietro");
                     CustomerDAO cd = new CustomerDAO();
                     input = new Scanner(System.in);
@@ -34,14 +35,22 @@ public class LabSWE {
                                 case 1 -> {
                                     System.out.println("Inserire codice cliente:");
                                     input = new Scanner(System.in);
-                                    cd.updateCustomerInfo(input.nextInt());
+                                    Customer c = cd.findById(input.nextInt());
+                                    clientOptions(c);
+                                    //TODO inserire il codice per modificare il database in base ai nuovi valori contenuti in c: il metodo dovrà essere contenuto in CustomerDAO
                                 }
                                 case 2 -> {
                                     System.out.println("Inserire dati cliente:");
                                     input = new Scanner(System.in);
-                                    cd.updateCustomerInfo(input.nextLine());
+                                    String fullName = input.nextLine();
+                                    input = new Scanner(System.in);
+                                    String email = input.nextLine();
+                                    Customer c = cd.findByInfo(fullName, email);
+                                    clientOptions(c);
+                                    //TODO inserire il codice per modificare il database in base ai nuovi valori contenuti in c: il metodo dovrà essere contenuto in CustomerDAO
                                 }
                                 case 3 -> {
+                                    //TODO forse per tornare alla pagina precedente serve un booleano
                                 }
                                 default -> System.out.println("Opzione non valida...");
                             }
@@ -59,6 +68,38 @@ public class LabSWE {
             System.out.println("\n");
         }
 
+    }
+
+    private static void clientOptions(Customer c) {
+        System.out.println("Vuoi eseguire delle modifiche al cliente #" + c.get_customerID() + "? (Y/N)");
+        Scanner input = new Scanner(System.in);
+        String line = input.nextLine();
+        if ("Y".equals(line) || "y".equals(line)) {
+            boolean modifying = true;
+            while (modifying) {
+                System.out.println("Cosa vuoi modificare?");
+                System.out.println("\t 1 - Nome");
+                System.out.println("\t 2 - Cognome");
+                System.out.println("\t 3 - Indirizzo e-mail");
+                System.out.println("\t 4 - Torna indietro");
+                input = new Scanner(System.in);
+                switch (input.nextInt()) {
+                    case 1 -> {
+                        input = new Scanner(System.in);
+                        c.set_first_name(input.nextLine());
+                    }
+                    case 2 -> {
+                        input = new Scanner(System.in);
+                        c.set_last_name(input.nextLine());
+                    }
+                    case 3 -> {
+                        input = new Scanner(System.in);
+                        c.set_email(input.nextLine());
+                    }
+                    case 4 -> modifying = false;
+                }
+            }
+        }
     }
 }
 
