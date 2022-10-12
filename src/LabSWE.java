f15ad966a733a0d5babb7cc3b7b8094a14b0e7f0
FilippoSciammacca <filipposciammacca@libero.it>
2022-10-12 15:36:53 +0200

    Classe LabSWE: aggiornata la CLI

--- src/LabSWE.java
+++ src/LabSWE.java
@@ -10,19 +10,48 @@ public class LabSWE {
         while(running) {
             System.out.println("Scegli un'opzione:");
             //System.out.println("\t -");
-            System.out.println("\t 1 - Aggiungi un nuovo cliente");
-            System.out.println("\t 2 - Effettua una prenotazione");
+            System.out.println("\t 1 - Operazioni Cliente");
+            System.out.println("\t 2 - Operazioni Prenotazione");
             System.out.println("\t 3 - Esci");
             Scanner input = new Scanner(System.in);
             switch (input.nextInt()) {
                 case 1 -> {
-                    System.out.println("Nuovo cliente");
+                    System.out.println("\t-- CLIENTE --");
+                    System.out.println("\t 1 - Aggiungi nuovo cliente");
+                    System.out.println("\t 2 - Modifica dati cliente");
+                    System.out.println("\t 3 - Torna indietro");
                     CustomerDAO cd = new CustomerDAO();
-                    cd.addNewCustomer();
+                    input = new Scanner(System.in);
+                    switch (input.nextInt()) {
+                        case 1 -> cd.addNewCustomer();
+                        case 2 -> {
+                            System.out.println("\t Cercare per:");
+                            System.out.println("\t 1 - codice cliente");
+                            System.out.println("\t 2 - dati cliente");
+                            System.out.println("\t 3 - Torna indietro");
+                            input = new Scanner(System.in);
+                            switch (input.nextInt()) {
+                                case 1 -> {
+                                    System.out.println("Inserire codice cliente:");
+                                    input = new Scanner(System.in);
+                                    cd.updateCustomerInfo(input.nextInt());
+                                }
+                                case 2 -> {
+                                    System.out.println("Inserire dati cliente:");
+                                    input = new Scanner(System.in);
+                                    cd.updateCustomerInfo(input.nextLine());
+                                }
+                                case 3 -> {
+                                }
+                                default -> System.out.println("Opzione non valida...");
+                            }
+                        }
+                        default -> System.out.println("Opzione non valida...");
+                    }
                 }
-                case 2 -> System.out.println("Nuova prenotazione");
+                case 2 -> System.out.println("-- PRENOTAZIONE --");
                 case 3 -> {
-                    System.out.println("Chiudi programma");
+                    System.out.println("-- CHIUSURA PROGRAMMA --");
                     running = false;
                 }
                 default -> System.out.println("Opzione non valida...");
