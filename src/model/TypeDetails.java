c3a8153d6ffb0a74ecdb28ac94630040f4b2ce42
FilippoSciammacca <filipposciammacca@libero.it>
2022-10-14 17:15:51 +0200

    Aggiunta la classe TypeDetails per i dettagli degli ombrelloni; Aggiunta la classe UmbrellaType che istanzia una tabella contenente una mappa per le informazioni sui tipi degli ombrelloni

---
+++ src/model/TypeDetails.java
@@ -0,0 +1,22 @@
+package model;
+
+public class TypeDetails{
+    private String typeName;
+    private float typePrice;
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public float getTypePrice() {
+        return typePrice;
+    }
+
+    public void setTypePrice(float typePrice) {
+        this.typePrice = typePrice;
+    }
+}
