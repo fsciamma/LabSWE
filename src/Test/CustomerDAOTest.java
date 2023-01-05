e6832805f92ddccc7e0d2b4d73adaa3d4c0a1213
Niccolo <niccolo.menghini@stud.unifi.it>
2023-01-05 16:08:55 +0100

    Impostata libreria Jtest

---
+++ src/Test/CustomerDAOTest.java
@@ -0,0 +1,18 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerDAOTest {
+
+    @org.junit.jupiter.api.Test
+    void addNewCustomer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findByEMail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateInfo() {
+    }
+}
